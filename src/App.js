5fa0bb221cc3621bce0313b6c5c27878810afd21
Nareshshankar A <naresh161101@gmail.com>
2023-09-05 18:14:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import Forecast from './Components/Weather-app/Forecast';
+
+
+function App() {
+  return (
+  <div className='App'>
+ <Forecast />
+  </div>
+  )
+      
+}
+
+export default App;
